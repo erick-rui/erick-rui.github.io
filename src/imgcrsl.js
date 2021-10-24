440b8205095de72e3bcbf72e6f277d53c09a614f
Erick Ruilova <erick.123ruilova@gmail.com>
2021-10-24 16:34:56 -0400

    Update imgcrsl.js

--- src/imgcrsl.js
+++ src/imgcrsl.js
@@ -1,28 +1,43 @@
 import ItemsCarousel from 'react-items-carousel';
- 
+import styled from 'styled-components';
+import { Button, Icon, Col } from 'antd';
+import { createImageChildren } from './CarouselSlideItem';
+
+const noOfItems = 7;
+const noOfCards = 2;
+const chevronWidth = 60;
+
+const Wrapper = styled.div`
+  padding: 0 20px;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+const carouselItems = createImageChildren(noOfItems);
+
 export default () => {
   const [activeItemIndex, setActiveItemIndex] = React.useState(0);
-  const chevronWidth = 40;
   return (
-    <div style={{ padding: `0 ${chevronWidth}px` }}>
+    <Wrapper>
       <ItemsCarousel
-        requestToChangeActive={setActiveItemIndex}
+        infiniteLoop
+        gutter={12}
+        numberOfCards={noOfCards}
         activeItemIndex={activeItemIndex}
-        numberOfCards={2}
-        gutter={20}
+        requestToChangeActive={setActiveItemIndex}
         leftChevron={<button>{'<'}</button>}
         rightChevron={<button>{'>'}</button>}
-        outsideChevron
         chevronWidth={chevronWidth}
+        children={carouselItems}
       >
-        <div style={{ height: 200, background: '#EEE' }}>First card</div>
+       <div style={{ height: 200, background: '#EEE' }}>First card</div>
         <div style={{ height: 200, background: '#EEE' }}>Second card</div>
         <div style={{ height: 200, background: '#EEE' }}>Third card</div>
         <div style={{ height: 200, background: '#EEE' }}>Fourth card</div>
-      </ItemsCarousel>
-    </div>
+        </ItemsCarousel>
+    </Wrapper>
   );
 };
 
 const domContainer = document.querySelector('#carousel'); 
-ReactDOM.render(e(ItemsCarousel), domContainer);
+ReactDOM.render(React.createElement(ItemsCarousel), domContainer);
