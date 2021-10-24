3ca519be4ab58c60d09fda6f35d89d1c03d656d2
Erick Ruilova <erick.123ruilova@gmail.com>
2021-10-24 15:30:24 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Carousel } from '@trendyol-js/react-carousel';
+import { Item } from './yourItem';
+
+ReactDOM.render(
+	<Carousel>
+		<Item />
+		<Item />
+		<Item />
+		<Item />
+	</Carousel>,
+	document.getElementById('root'),
+);
