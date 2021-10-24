b75243e178df3742d1d32acc16c7bde593f076c4
Erick Ruilova <erick.123ruilova@gmail.com>
2021-10-24 16:58:12 -0400

    debugging

--- src/imgcrsl.js
+++ src/imgcrsl.js
@@ -1,43 +1,71 @@
-import ItemsCarousel from 'react-items-carousel';
+import range from 'lodash/range';
 import styled from 'styled-components';
-import { Button, Icon, Col } from 'antd';
-import { createImageChildren } from './CarouselSlideItem';
+import ItemsCarousel from '../../src/ItemsCarousel';
 
-const noOfItems = 7;
-const noOfCards = 2;
-const chevronWidth = 60;
+const noOfItems = 12;
+const noOfCards = 3;
+const autoPlayDelay = 2000;
+const chevronWidth = 40;
 
 const Wrapper = styled.div`
-  padding: 0 20px;
+  padding: 0 ${chevronWidth}px;
   max-width: 1000px;
   margin: 0 auto;
 `;
 
-const carouselItems = createImageChildren(noOfItems);
+const SlideItem = styled.div`
+  height: 200px;
+  background: #EEE;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  font-weight: bold;
+`;
 
-export default () => {
-  const [activeItemIndex, setActiveItemIndex] = React.useState(0);
-  return (
-    <Wrapper>
-      <ItemsCarousel
-        infiniteLoop
-        gutter={12}
-        numberOfCards={noOfCards}
-        activeItemIndex={activeItemIndex}
-        requestToChangeActive={setActiveItemIndex}
-        leftChevron={<button>{'<'}</button>}
-        rightChevron={<button>{'>'}</button>}
-        chevronWidth={chevronWidth}
-        children={carouselItems}
-      >
-       <div style={{ height: 200, background: '#EEE' }}>First card</div>
-        <div style={{ height: 200, background: '#EEE' }}>Second card</div>
-        <div style={{ height: 200, background: '#EEE' }}>Third card</div>
-        <div style={{ height: 200, background: '#EEE' }}>Fourth card</div>
-        </ItemsCarousel>
-    </Wrapper>
-  );
-};
+const carouselItems = range(noOfItems).map(index => (
+  <SlideItem key={index}>
+    {index+1}
+  </SlideItem>
+));
+
+class AutoPlayCarousel extends React.Component {
+  state = {
+    activeItemIndex: 0,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(this.tick, autoPlayDelay);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick = () => this.setState(prevState => ({
+    activeItemIndex: (prevState.activeItemIndex + 1) % (noOfItems-noOfCards + 1),
+  }));
+
+  onChange = value => this.setState({ activeItemIndex: value });
+
+  render() {
+    return (
+      <Wrapper>
+        <ItemsCarousel
+          gutter={12}
+          numberOfCards={noOfCards}
+          activeItemIndex={this.state.activeItemIndex}
+          requestToChangeActive={this.onChange}
+          rightChevron={<button>{'>'}</button>}
+          leftChevron={<button>{'<'}</button>}
+          chevronWidth={chevronWidth}
+          outsideChevron
+          children={carouselItems}
+        />
+      </Wrapper>
+    );
+  }
+}
 
 const domContainer = document.querySelector('#carousel'); 
-ReactDOM.render(React.createElement(ItemsCarousel), domContainer);
+ReactDOM.render(e(myButton), domContainer);
